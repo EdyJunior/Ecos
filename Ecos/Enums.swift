c58cf6d650d18efd8585e9fcbd4bb01e6094d95d
EdyJunior <dujunior1300@gmail.com>
2016-10-07 08:16:48 -0300

    first commit

---
+++ Ecos/Enums.swift
@@ -0,0 +1,9 @@
+//
+//  Enums.swift
+//  Ecos
+//
+//  Created by Edvaldo Junior on 06/10/16.
+//  Copyright © 2016 Edvaldo Junior. All rights reserved.
+//
+
+import Foundation
---
+++ Ecos/FirstScene.swift
@@ -0,0 +1,13 @@
+//
+//  FirstScene.swift
+//  Ecos
+//
+//  Created by Edvaldo Junior on 06/10/16.
+//  Copyright © 2016 Edvaldo Junior. All rights reserved.
+//
+
+import UIKit
+
+class FirstScene: SKScene {
+
+}
