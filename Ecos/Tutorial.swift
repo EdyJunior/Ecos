94cf74a692916529bae9661ce8d3ccf4400315dd
Edvaldo Junior <dujunior1300@gmail.com>
2016-10-31 16:24:50 -0300

    fix tutorial in GameScene

--- Ecos/Tutorial.swift
+++ Ecos/Tutorial.swift
@@ -9,69 +9,68 @@
 import SpriteKit
 
 class Tutorial: NSObject {
-    
+
     var scene = GameScene()
     var hand = SKSpriteNode(imageNamed: "hand")
     var blackBack = SKSpriteNode(imageNamed: "blackBack")
     var text = InfoLabel()
     var action: ((Button)->Void)?
     var zPos: CGFloat = 0
-    
+
     init(withScene scene: GameScene) {
-        
+
         self.scene = scene
         blackBack.alpha = 0.7
         blackBack.size = scene.size
         super.init()
     }
-    
+
     func start(button: Button, text: String) {
-        
-        self.text = InfoLabel(text: text)
-        self.text.set(fontSize: scene.size.height * 0.1, position: scene.player.position, fontColor: .white, zPosition: .highlighted)
+
+        let pos = button.touchableArea.position
         
         hand.zPosition = Position.highlighted.rawValue
-        hand.size = CGSize(width: scene.size.width * 0.2, height: scene.size.height * 0.3)
+        hand.size = CGSize(width: scene.size.width * 0.3, height: scene.size.height * 0.2)
+        hand.position = CGPoint(x: -hand.size.width/2, y: pos.y)
         
-        let pos = button.touchableArea.position
+        self.text = InfoLabel(text: text)
+        
         if pos.y > 0 {
-            hand.position = CGPoint(x: 0, y: -hand.size.height / 2 - button.touchableArea.size.height / 2)
-            hand.yScale *= (hand.yScale > 0 ? 1 : -1)
-            self.text.position = CGPoint(x: -scene.size.width/3, y: hand.position.y - self.text.fontSize)
+            let textPos = CGPoint(x: 0, y: pos.y - hand.size.height)
+            self.text.set(fontSize: scene.size.height * 0.1, position: textPos, fontColor: .white, zPosition: .highlighted)
         } else {
-            hand.position = CGPoint(x: 0, y: hand.size.height / 2 + button.touchableArea.size.height / 2)
-            hand.yScale *= (hand.yScale > 0 ? -1 : 1)
-            self.text.position = CGPoint(x: -scene.size.width/3, y: hand.position.y + self.text.fontSize)
+            let textPos = CGPoint(x: 0, y: pos.y + hand.size.height)
+            self.text.set(fontSize: scene.size.height * 0.1, position: textPos, fontColor: .white, zPosition: .highlighted)
         }
         
         blackBack.position = CGPoint(x: scene.player.position.x, y: scene.size.height / 2)
         blackBack.zPosition = Position.front.rawValue
-        
-        button.touchableArea.addChild(hand)
-        button.touchableArea.addChild(self.text)
+
+        scene.camera!.addChild(hand)
+        scene.camera!.addChild(self.text)
         scene.addChild(blackBack)
-       
+
         self.zPos = button.zPosition
         button.zPosition = Position.highlighted.rawValue
-        
+
         scene.player.removeAllActions()
-        
+
         self.action = button.action
         button.action = end
         button.enabled = true
     }
-    
+
     func end(button: Button) {
 
-        button.touchableArea.removeChildren(in: [hand, text])
+        scene.camera!.removeChildren(in: [hand, text])
         scene.removeChildren(in: [blackBack])
         button.touchableArea.zPosition = self.zPos
         self.action?(button)
         scene.player.walk()
     }
-    
+
     func tutorialTrigger(triggerPosition: CGPoint, buttonType: BodyType, room: Room) {
-        
+
         let trigger = SKSpriteNode()
         trigger.size = CGSize(width: scene.size.width * 0.01, height: scene.size.height)
         trigger.position = triggerPosition
