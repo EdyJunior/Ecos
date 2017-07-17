1da140fca521dbb24a8796d9353ae470b7825409
Edvaldo Junior <dujunior1300@gmail.com>
2017-07-17 11:40:55 -0300

    refactor: remove FPS and number of nodes of the GameScene

--- Ecos/GameViewController.swift
+++ Ecos/GameViewController.swift
@@ -22,9 +22,6 @@ class GameViewController: UIViewController {
             view.presentScene(scene)
             
             view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
         }
     }
 
