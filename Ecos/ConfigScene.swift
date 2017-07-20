cf23641b305afb8649e5442b355eda29cc997fbe
Edvaldo Junior <dujunior1300@gmail.com>
2017-07-20 15:29:18 -0300

    style: change font

---
+++ Ecos/ConfigScene.swift
@@ -0,0 +1,13 @@
+//
+//  ConfigScene.swift
+//  Ecos
+//
+//  Created by Edvaldo Junior on 18/07/17.
+//  Copyright © 2017 Edvaldo Junior. All rights reserved.
+//
+
+import UIKit
+
+class ConfigScene: SKScene {
+
+}
