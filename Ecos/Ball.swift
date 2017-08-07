b16df9619fd7d379c5312b3579f506847ddad6be
Edvaldo Junior <dujunior1300@gmail.com>
2017-08-07 10:10:06 -0300

    feat(ball): create the ball(mobile button)

---
+++ Ecos/Ball.swift
@@ -0,0 +1,33 @@
+//
+//  Ball.swift
+//  Ecos
+//
+//  Created by Edvaldo Junior on 20/07/17.
+//  Copyright © 2017 Edvaldo Junior. All rights reserved.
+//
+
+import SpriteKit
+
+class Ball: Button {
+    
+    func initialize() {
+        
+        touchableArea.physicsBody = SKPhysicsBody(circleOfRadius: touchableArea.size.height / 7)
+        touchableArea.physicsBody?.allowsRotation = true
+        touchableArea.physicsBody?.categoryBitMask = BodyType.toy.rawValue
+        touchableArea.physicsBody?.affectedByGravity = true
+        touchableArea.physicsBody?.isDynamic = true
+        touchableArea.physicsBody?.restitution = 1
+        touchableArea.physicsBody?.collisionBitMask = BodyType.player.rawValue | BodyType.ground.rawValue | BodyType.limit.rawValue
+        touchableArea.physicsBody?.contactTestBitMask = 0
+        
+        touchableArea.xScale *= -1
+        touchableArea.zPosition = Position.middle.rawValue
+    }
+    
+    func touchBall(ball: Button) {
+        
+        let gameScene = self.scene as! GameScene
+        gameScene.player.jump()
+    }
+}
