84c1769b43633088d427266e7c1772e30edaa570
Edvaldo Junior <dujunior1300@gmail.com>
2017-08-16 19:00:01 -0300

    refactor: create class WaterTap

--- Ecos/WaterTap.swift
+++ Ecos/WaterTap.swift
@@ -10,4 +10,36 @@ import UIKit
 
 class WaterTap: Button {
 
+    init(tapPosition: CGPoint, sizeTap: CGSize) {
+        
+        super.init(defaultButtonImage: "WaterTap0", activeButtonImage: "WaterTap0")
+        self.action = self.touchWaterTap
+        super.setSizeAndPosition(sizeTap, position: tapPosition, areaFactor: 1.5)
+        super.animate("WaterTaps", imgName: "WaterTap")
+        super.zPosition = Position.before.rawValue
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func touchWaterTap(waterTap: Button) {
+        
+        let gameScene = self.scene as! GameScene
+        gameScene.updateScore(scoreToAdd: ScoreTable.waterTap)
+        waterTap.action = nil
+        
+        if let value = defaults.object(forKey: Key.nWaterTap.rawValue) {
+            var number = value as! Int
+            number += 1
+            defaults.set(number, forKey: Key.nWaterTap.rawValue)
+
+            let unlock = UnlockValues()
+            if number >= unlock.waterTap {
+                defaults.set(false, forKey: Key.waterTapCard.rawValue)
+            }
+        } else {
+            defaults.set(Int(1), forKey: Key.nWaterTap.rawValue)
+        }
+    }
 }
